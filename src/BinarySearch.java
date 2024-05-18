4df9534f11df688e5fb110e86829c07fe7916222
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2024-05-18 19:45:42 +0300

    !

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int searchKey = 23;
+        int result = binarySearch(array, searchKey);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+
+    public static int binarySearch(int[] a, int search) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (a[mid] == search) {
+                return mid;
+            } else if (a[mid] < search) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
 }
